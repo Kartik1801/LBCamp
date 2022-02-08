5b54cccb8987584b2dadd88c70ad706994bc9901
kartik1801 <kartikdhawan1801@gmail.com>
2022-02-08 15:48:28 +0530

    Added Login

--- routes/user.js
+++ routes/user.js
@@ -1,15 +1,35 @@
-((express, router, User, wrapAsync, generateError) => {
+((express, router, User, wrapAsync, passport) => {
+    
     router.get('/register', (req, res) => {
         res.render("users/register");
     })
     router.post('/register',wrapAsync(async (req, res) => {
-        res.send(req.body)
+        try{
+                const { user:userData } = req.body;
+                const user = new User({username: userData.username, email: userData.email});
+                const registeredUser = await User.register(user, userData.password);
+            }
+        catch(e){
+                req.flash("error", e.message);
+                return res.redirect("/register");
+            }
+        req.flash('success',"Welcome to LBCamp!")
+        res.redirect("/campgrounds");
     }))
+
+    router.get('/login', (req, res) => {
+        res.render("users/login");
+    })
+    router.post('/login', passport.authenticate('local', { failureFlash: true, failureRedirect: "/login" }), wrapAsync(async (req, res) => {
+        req.flash('success',"Welcome Back!")
+        res.redirect("/campgrounds");
+    }))
+
     module.exports = router;
 })(
     require("express"),
     require("express").Router(),
     require("../models/user"),
     require('../utilities/wrapAsync'),
-    require('../utilities/generateError')
+    require('passport')
 )

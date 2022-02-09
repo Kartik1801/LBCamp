2756e6f1c8f69677936bc69730ce2af803325941
kartik1801 <kartikdhawan1801@gmail.com>
2022-02-09 21:35:09 +0530

    updated

--- models/review.js
+++ models/review.js
@@ -1,7 +1,11 @@
 ((mongoose, Schema) => {
     const reviewSchema = new Schema({
         body: String,
-        rating: Number
+        rating: Number,
+        author: {
+            type: Schema.Types.ObjectId,
+            ref: 'User'
+        }
     })
     const Review =  mongoose.model('Review', reviewSchema)
     module.exports = Review;

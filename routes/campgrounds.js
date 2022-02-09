fab38dc8d4f52d48788f1ab1c551936629a06965
kartik1801 <kartikdhawan1801@gmail.com>
2022-02-09 22:44:21 +0530

    Added campgrounds Controller

--- routes/campgrounds.js
+++ routes/campgrounds.js
@@ -1,80 +1,23 @@
-((express, router, generateError, wrapAsync, joi, {campgroundSchema}, mongoose,  Campground, {isLoggedIn, isAuthor, validateCampground}) => {
-// Add a new Campground: 
-    router.get('/new', isLoggedIn, (req, res) => {
-        res.render("campgrounds/new");
-    });
-    router.post("/", isLoggedIn, validateCampground, wrapAsync(async (req, res, next) => {
-        if (!req.body.campgrounds) throw new generateError(400, "Missing/Invalid campgrounds Data.");         
-        const camp = new Campground(req.body.campgrounds);
-        camp.author = req.user._id;
-        await camp.save();
-        req.flash("success", 'Successfully Added a new Campground!');
-        res.redirect("/campgrounds");
-    }));    
-// Remove a Campground:
-    router.delete("/:id", isLoggedIn, isAuthor, wrapAsync(async (req, res, next) => {
-        const { id } = req.params;
-        if (!id) throw new generateError(400, "Missing/Invalid ID.")
-        await Campground.findByIdAndDelete(id);
-        req.flash("success", 'Successfully Deleted the Campground!');
-        res.redirect(`/campgrounds`)
-    }));
-// Edit a Campground: 
-    router.get('/:id/edit', isLoggedIn, isAuthor, wrapAsync(async (req, res, next) => {
-        const { id } = req.params;
-        if (!id) throw new generateError(400, "Missing/Invalid ID.")
-        const campground = await Campground.findById(id);
-        if (!campground) 
-            {   
-                req.flash('error',"Campground not Found!")
-                return res.redirect("/campgrounds")
-            }    
-        res.render("campgrounds/edit",{campground:campground});
-    }));
-    router.put("/:id", isLoggedIn, isAuthor, validateCampground, wrapAsync(async (req, res, next) => {
-            const { id } = req.params;
-            if (!id) throw new generateError(400, "Missing/Invalid ID.")
-            const campground = await Campground.findByIdAndUpdate(id,{...req.body.campgrounds})
-            if (!campground) 
-            {   
-                req.flash('error',"Campground not Found!")
-                return res.redirect("/campgrounds")
-            }           
-            req.flash("success", 'Successfully Updated the Campground!');
-            res.redirect(`/campgrounds/${id}`)
-    }));
-// Show Campground Details:
-    router.get('/:id', wrapAsync(async (req, res, next) => {
-            const {id} = req.params;
-            if (!id) throw new generateError(400, "Missing/Invalid ID.")
-            const campground = await Campground.findById(id).populate({
-                 path:"reviews",
-                 populate: { 
-                     path: "author"
-                    }
-                }).populate('author','username email');
-            if (!campground) 
-            {   
-                req.flash('error',"Campground not Found!")
-                return res.redirect("/campgrounds")
-            }    
-            res.render("campgrounds/show",{campground})
-    }));
-// Show All Campgrounds:
-    router.get('/',wrapAsync(async (req, res, next) => {
-            const campground = await Campground.find({});
-            if (!campground) throw new generateError(404, "No Data found")
-            res.render("campgrounds/index",{campgrounds:campground})
-    }));
+((router, wrapAsync,  {isLoggedIn, isAuthor, validateCampground}, campground ) => {
+    
+    router.get('/',wrapAsync(campground.index));
+
+    router.get('/new', isLoggedIn, campground.renderNewForm);
+
+    router.post('/', isLoggedIn, validateCampground, wrapAsync(campground.createCampground));    
+    
+    router.get('/:id/edit', isLoggedIn, isAuthor, wrapAsync(campground.renderEditForm));
+    
+    router.get('/:id', wrapAsync(campground.showCampground));
+    
+    router.put('/:id', isLoggedIn, isAuthor, validateCampground, wrapAsync(campground.editCampground));
+    
+    router.delete('/:id', isLoggedIn, isAuthor, wrapAsync(campground.deleteCampground));
+    
     module.exports = router;
 })(
-    require("express"),
     require("express").Router(),
-    require('../utilities/generateError'),
     require('../utilities/wrapAsync'),
-    require('joi'),
-    require('../schemas'), 
-    require('mongoose'),
-    require('../models/campground'),
-    require('../middleware')
+    require('../middleware'),
+    require('../controllers/campgrounds')
 )

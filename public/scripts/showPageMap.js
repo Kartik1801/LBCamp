d887e26a44d9577703d5d25aa43eb4da86b43c68
kartik1801 <kartikdhawan1801@gmail.com>
2022-02-14 00:12:43 +0530

    Updated map

--- public/scripts/showPageMap.js
+++ public/scripts/showPageMap.js
@@ -2,10 +2,16 @@ console.log(campground)
 mapboxgl.accessToken = mapToken;
 const map = new mapboxgl.Map({
   container: 'map', // container ID
-  style: 'mapbox://styles/mapbox/streets-v11', // style URL
+  style: 'mapbox://styles/mapbox/light-v10', // style URL
   center: campground.geometry.coordinates, // centerstarting position [lng, lat]
   zoom: 10 // starting zoom
   });
  const marker1 = new mapboxgl.Marker()
  .setLngLat(campground.geometry.coordinates)
+ .setPopup(
+     new mapboxgl.Popup({offset: 25})
+     .setHTML(
+         `<h3>${campground.title}</h3><p>${campground.location}</p>`
+     )
+ )
  .addTo(map);

fc00d81e0f8b215f71ea109461b0dbc3f783d412
kartik1801 <kartikdhawan1801@gmail.com>
2022-02-14 22:12:55 +0530

    Added virtual property for custom Popup

--- models/campground.js
+++ models/campground.js
@@ -3,14 +3,19 @@
             url: String,
             filename: String
     })
+    // added a virtual function that returns url of an image with w_200 in its url 
+    // which is then used to obtain image of size 200px from cloudinary
     ImageSchema.virtual('thumbnail').get(
       function() {return this.url.replace('/upload', '/upload/w_200')}
     )
+    // By default when a mongoDB schema is JSON.stingified then it doesnt include virtual property
+    // so to include them we need to add option to our schema 
+    const options = {toJSON: { virtuals: true}}
     const CampgroundSchema = new Schema({
         title: String,
         price: Number,
         images: [
-           ImageSchema
+            ImageSchema
         ],
         geometry:{
             type: {
@@ -35,7 +40,11 @@
                 ref: "Review"
             }
         ]
-    })
+    }, options)
+    CampgroundSchema.virtual('properties.popUpMarkup').get(function(){
+        return `<strong><a href="/campgrounds/${this._id}">${this.title}</a></strong>
+        <p>${this.description.substr(0,20)}...</p>`
+    })   
     CampgroundSchema.post('findOneAndDelete', async function (camp){
         if(camp){
             await Review.deleteMany({
--- public/scripts/clusterMap.js
+++ public/scripts/clusterMap.js
@@ -1,20 +1,20 @@
 mapboxgl.accessToken = mapToken;
 const map = new mapboxgl.Map({
 container: 'map',
-style: 'mapbox://styles/mapbox/dark-v10',
+style: 'mapbox://styles/mapbox/light-v10',
 center: [-103.5917, 40.6699],
-zoom: 3
+zoom: 1
 });
  
 map.on('load', () => {
 // Add a new source from our GeoJSON data and
 // set the 'cluster' option to true. GL-JS will
 // add the point_count property to your source data.
-map.addSource('earthquakes', {
+map.addSource('campgrounds', {
 type: 'geojson',
 // Point to GeoJSON data. This example visualizes all M1.0+ earthquakes
 // from 12/22/15 to 1/21/16 as logged by USGS' Earthquake hazards program.
-data: 'https://docs.mapbox.com/mapbox-gl-js/assets/earthquakes.geojson',
+data: campgrounds,
 cluster: true,
 clusterMaxZoom: 14, // Max zoom to cluster points on
 clusterRadius: 50 // Radius of each cluster when clustering points (defaults to 50)
@@ -23,7 +23,7 @@ clusterRadius: 50 // Radius of each cluster when clustering points (defaults to 
 map.addLayer({
 id: 'clusters',
 type: 'circle',
-source: 'earthquakes',
+source: 'campgrounds',
 filter: ['has', 'point_count'],
 paint: {
 // Use step expressions (https://docs.mapbox.com/mapbox-gl-js/style-spec/#expressions-step)
@@ -34,20 +34,20 @@ paint: {
 'circle-color': [
 'step',
 ['get', 'point_count'],
-'#51bbd6',
-100,
-'#f1f075',
-750,
-'#f28cb1'
+'#00BCD4',
+10,
+'#2196F3',
+30,
+'#3F51B5'
 ],
 'circle-radius': [
 'step',
 ['get', 'point_count'],
+15,
+10,
 20,
-100,
 30,
-750,
-40
+25
 ]
 }
 });
@@ -55,7 +55,7 @@ paint: {
 map.addLayer({
 id: 'cluster-count',
 type: 'symbol',
-source: 'earthquakes',
+source: 'campgrounds',
 filter: ['has', 'point_count'],
 layout: {
 'text-field': '{point_count_abbreviated}',
@@ -67,11 +67,11 @@ layout: {
 map.addLayer({
 id: 'unclustered-point',
 type: 'circle',
-source: 'earthquakes',
+source: 'campgrounds',
 filter: ['!', ['has', 'point_count']],
 paint: {
 'circle-color': '#11b4da',
-'circle-radius': 4,
+'circle-radius': 10,
 'circle-stroke-width': 1,
 'circle-stroke-color': '#fff'
 }
@@ -83,7 +83,7 @@ const features = map.queryRenderedFeatures(e.point, {
 layers: ['clusters']
 });
 const clusterId = features[0].properties.cluster_id;
-map.getSource('earthquakes').getClusterExpansionZoom(
+map.getSource('campgrounds').getClusterExpansionZoom(
 clusterId,
 (err, zoom) => {
 if (err) return;
@@ -102,10 +102,7 @@ zoom: zoom
 // description HTML from its properties.
 map.on('click', 'unclustered-point', (e) => {
 const coordinates = e.features[0].geometry.coordinates.slice();
-const mag = e.features[0].properties.mag;
-const tsunami =
-e.features[0].properties.tsunami === 1 ? 'yes' : 'no';
- 
+const text = e.features[0].properties.popUpMarkup;
 // Ensure that if the map is zoomed out such that
 // multiple copies of the feature are visible, the
 // popup appears over the copy being pointed to.
@@ -116,7 +113,7 @@ coordinates[0] += e.lngLat.lng > coordinates[0] ? 360 : -360;
 new mapboxgl.Popup()
 .setLngLat(coordinates)
 .setHTML(
-`magnitude: ${mag}<br>Was there a tsunami?: ${tsunami}`
+    text
 )
 .addTo(map);
 });

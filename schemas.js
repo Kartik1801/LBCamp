c74e533ca68dfb562e5e8881a33bb85ebaa2e6b5
kartik1801 <kartikdhawan1801@gmail.com>
2022-02-15 22:22:58 +0530

    Updated to avoid XSS

--- schemas.js
+++ schemas.js
@@ -1,19 +1,40 @@
-const joi = require('joi')
+const basejoi = require('joi')
+const sanitizeHtml = require('sanitize-html')
+const extension = (joi) => ({
+    type: 'string',
+    base: joi.string(),
+    messages: {
+        'string.escapeHTML': '{{#label}} must not include HTML!'
+    },
+    rules:{
+        escapeHTML: {
+            validate(value, helpers){
+                const clean = sanitizeHtml(value, {
+                    allowedTags: [],
+                    allowedAttributes: {}
+                });
+                if (clean !== value) return helpers.error('string.escapeHTML', {value})
+                return clean;
+            }
+        }
+    }
+});
+const joi = basejoi.extend(extension)
 // Using JOI to setup a schema for campgrounds:
 module.exports.campgroundSchema = joi.object({
     campgrounds: joi.object({
-        title: joi.string().required(),
+        title: joi.string().required().escapeHTML(),
         price: joi.number().required().min(0),
         /* images: joi.string().required(), */
-        description: joi.string().required(),
-        location: joi.string().required()
+        description: joi.string().required().escapeHTML(),
+        location: joi.string().required().escapeHTML()
     }).required(),
     deleteImages: joi.array()
 });
 
 module.exports.reviewSchema = joi.object({
     review: joi.object({
-        body: joi.string().required(),
+        body: joi.string().required().escapeHTML(),
         rating: joi.number().required().min(1).max(5)
     }).required()
 })

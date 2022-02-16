15cd59889191f115daadf183abed4ff11bc2d4dd
kartik1801 <kartikdhawan1801@gmail.com>
2022-02-16 22:22:46 +0530

    Fixed helmet

--- app.js
+++ app.js
@@ -1,10 +1,10 @@
-((express, app, path, mongoose, methodOverride, ejsMate, generateError, campgroundRoutes, reviewRoutes, session, flash, passport, passportLocal, User, userRoutes, expressMongoSanitize, helmet) => {
+((express, app, path, mongoose, methodOverride, ejsMate, generateError, campgroundRoutes, reviewRoutes, session, flash, passport, passportLocal, User, userRoutes, expressMongoSanitize, helmet, ) => {
     if(process.env.NODE_ENV !== 'production'){
         require('dotenv').config()
     }
     // MAKE SURE TO ENCODE YOUR DB_URL IF YOUR CREDENTIALS CONTAINS SPECIAL CHARACTERS
-    const dburl = process.env.DB_URL;
-    mongoose.connect(dburl);
+    /* const dburl = process.env.DB_URL; */
+    mongoose.connect('mongodb://localhost:27017/lb-camp');
     mongoose.connection.on("error", console.error.bind(console, "Connection Error"))
     mongoose.connection.once("open", () => console.log("Database Connected"));
 // Middlewares:
@@ -15,7 +15,6 @@
     app.use(methodOverride("_method"));
     app.use(express.urlencoded({extended: true}));
     app.use(express.static(path.join(__dirname, "public")));
-    
     const sessionConfig = {
         secret: "secret",
         resave: false,
@@ -35,6 +34,51 @@
    
     passport.serializeUser( User.serializeUser());
     passport.deserializeUser( User.deserializeUser());
+
+    const scriptSrcUrls = [
+        "https://stackpath.bootstrapcdn.com/",
+        "https://api.tiles.mapbox.com/",
+        "https://api.mapbox.com/",
+        "https://kit.fontawesome.com/",
+        "https://cdnjs.cloudflare.com/",
+        "https://cdn.jsdelivr.net",
+    ];
+    const styleSrcUrls = [
+        "https://kit-free.fontawesome.com/",
+        "https://stackpath.bootstrapcdn.com/",
+        "https://api.mapbox.com/",
+        "https://api.tiles.mapbox.com/",
+        "https://fonts.googleapis.com/",
+        "https://use.fontawesome.com/",
+        "https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css"
+    ];
+    const connectSrcUrls = [
+        "https://api.mapbox.com/",
+        "https://a.tiles.mapbox.com/",
+        "https://b.tiles.mapbox.com/",
+        "https://events.mapbox.com/",
+    ];
+    const fontSrcUrls = [];
+    app.use(
+        helmet.contentSecurityPolicy({
+            directives: {
+                defaultSrc: [],
+                connectSrc: ["'self'", ...connectSrcUrls],
+                scriptSrc: ["'unsafe-inline'", "'self'", ...scriptSrcUrls],
+                styleSrc: ["'self'", "'unsafe-inline'", ...styleSrcUrls],
+                workerSrc: ["'self'", "blob:"],
+                objectSrc: [],
+                imgSrc: [
+                    "'self'",
+                    "blob:",
+                    "data:",
+                    "https://res.cloudinary.com/dgj3fg9in/", //SHOULD MATCH YOUR CLOUDINARY ACCOUNT! 
+                    "https://images.unsplash.com/",
+                ],
+                fontSrc: ["'self'", ...fontSrcUrls],
+            },
+        })
+    );
 
     app.use((req, res, next) => {
         res.locals.currentUser = req.user;

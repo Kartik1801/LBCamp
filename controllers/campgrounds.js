0ff6d3b7cd4d8049bb1b841f4e8a7c0ae36a74b5
kartik1801 <kartikdhawan1801@gmail.com>
2022-02-24 01:14:45 +0530

    Refactored Code

--- controllers/campgrounds.js
+++ controllers/campgrounds.js
@@ -1,96 +1,94 @@
-((Campground, generateError, {cloudinary}, mbxGeocoding) => {
-    if(process.env.NODE_ENV !== 'production'){
-        require('dotenv').config()
-    }
-    const mapboxToken = process.env.MAPBOX_TOKEN;
-    const geocoder = mbxGeocoding({ accessToken: "sk.eyJ1Ijoia2QxODAxIiwiYSI6ImNrenp1cDN0NTAwem0zZG1zY2gzMTI5NWUifQ.A3Qp-nI6gAlpweINAVEJqA" || process.env.MAPBOX_TOKEN })
+const Campground = require('../models/campground');
+const generateError = require('../utilities/generateError');
+const {cloudinary} = require('../cloudinary/index');
+const mbxGeocoding = require('@mapbox/mapbox-sdk/services/geocoding'); 
 
-    module.exports.index = async (req, res, next) => {
-            const campground = await Campground.find({});
-            if (!campground) throw new generateError(404, "No Data found")
-            res.render("campgrounds/index",{campgrounds:campground})
-    }
+if(process.env.NODE_ENV !== 'production') require('dotenv').config();
+
+const geocoder = mbxGeocoding({ accessToken: process.env.MAPBOX_TOKEN || "sk.eyJ1Ijoia2QxODAxIiwiYSI6ImNrenp1cDN0NTAwem0zZG1zY2gzMTI5NWUifQ.A3Qp-nI6gAlpweINAVEJqA" || process.env.MAPBOX_TOKEN })
+
+module.exports.index = async (req, res, next) => {
+    const campground = await Campground.find({});
+    if (!campground) throw new generateError(404, "No Data found")
+    res.render("campgrounds/index",{campgrounds:campground})
+}
+module.exports.renderNewForm = (req, res) => {
+    res.render("campgrounds/new");
+}
+module.exports.createCampground = async (req, res, next) => {
+    if (!req.body.campgrounds) throw new generateError(400, "Missing/Invalid campgrounds Data.");         
+    const camp = new Campground(req.body.campgrounds);
+    const geodata =  await geocoder.forwardGeocode({
+            query: req.body.campgrounds.location,
+            limit: 1
+        }).send()
+    camp.geometry = geodata.body.features[0].geometry
+    camp.images = req.files.map(f => ({ url: f.path, filename: f.filename }))
+    camp.author = req.user._id;
+    await camp.save();
+    req.flash("success", 'Successfully Added a new Campground!');
+    res.redirect("/campgrounds");
+}    
+module.exports.deleteCampground = async (req, res, next) => {
+    const { id } = req.params;
+    if (!id) throw new generateError(400, "Missing/Invalid ID.")
+    await Campground.findByIdAndDelete(id);
+    req.flash("success", 'Successfully Deleted the Campground!');
+    res.redirect(`/campgrounds`)
+}
     
-    module.exports.renderNewForm = (req, res) => {
-            res.render("campgrounds/new");
-    }
-    module.exports.createCampground = async (req, res, next) => {
-            if (!req.body.campgrounds) throw new generateError(400, "Missing/Invalid campgrounds Data.");         
-            const camp = new Campground(req.body.campgrounds);
-            const geodata =  await geocoder.forwardGeocode({
-                    query: req.body.campgrounds.location,
-                    limit: 1
-                }).send()
-            camp.geometry = geodata.body.features[0].geometry
-            camp.images = req.files.map(f => ({ url: f.path, filename: f.filename }))
-            camp.author = req.user._id;
-            await camp.save();
-            req.flash("success", 'Successfully Added a new Campground!');
-            res.redirect("/campgrounds");
-    }
+module.exports.renderEditForm = async (req, res, next) => {
+    const { id } = req.params;
+    if (!id) throw new generateError(400, "Missing/Invalid ID.")
+    const campground = await Campground.findById(id);
+    if (!campground) 
+    {   
+        req.flash('error',"Campground not Found!")
+        return res.redirect("/campgrounds")
+    }    
+    res.render("campgrounds/edit",{campground:campground});
+}
     
-    module.exports.deleteCampground = async (req, res, next) => {
-            const { id } = req.params;
-            if (!id) throw new generateError(400, "Missing/Invalid ID.")
-            await Campground.findByIdAndDelete(id);
-            req.flash("success", 'Successfully Deleted the Campground!');
-            res.redirect(`/campgrounds`)
+module.exports.editCampground = async (req, res, next) => {
+    const { id } = req.params;
+    if (!id) throw new generateError(400, "Missing/Invalid ID.")
+    const geodata =  await geocoder.forwardGeocode({
+        query: req.body.campgrounds.location,
+        limit: 1
+    }).send()
+    const campground = await Campground.findByIdAndUpdate(id,{...req.body.campgrounds})
+    if (!campground) 
+    {   
+        req.flash('error',"Campground not Found!")
+        return res.redirect("/campgrounds")
+    }           
+    const imgs = req.files.map(f => ({ url: f.path, filename: f.filename }))
+    campground.geometry = geodata.body.features[0].geometry;
+    campground.images.push(...imgs);
+    await campground.save()
+    if(req.body.deleteImages)
+    {
+        for(let filename of req.body.deleteImages){
+            await cloudinary.uploader.destroy(filename)
+        }
+        await campground.updateOne({$pull: { images: { filename: {$in: req.body.deleteImages }}}})
     }
-    
-    module.exports.renderEditForm = async (req, res, next) => {
-            const { id } = req.params;
-            if (!id) throw new generateError(400, "Missing/Invalid ID.")
-            const campground = await Campground.findById(id);
-            if (!campground) 
-                {   
-                    req.flash('error',"Campground not Found!")
-                    return res.redirect("/campgrounds")
-                }    
-            res.render("campgrounds/edit",{campground:campground});
-    }
-    
-    module.exports.editCampground = async (req, res, next) => {
-            const { id } = req.params;
-            if (!id) throw new generateError(400, "Missing/Invalid ID.")
-            const campground = await Campground.findByIdAndUpdate(id,{...req.body.campgrounds})
-            if (!campground) 
-            {   
-                req.flash('error',"Campground not Found!")
-                return res.redirect("/campgrounds")
-            }           
-            const imgs = req.files.map(f => ({ url: f.path, filename: f.filename }))
-            campground.images.push(...imgs);
-            await campground.save()
-            if(req.body.deleteImages)
-            {
-                for(let filename of req.body.deleteImages){
-                    await cloudinary.uploader.destroy(filename)
-                }
-                await campground.updateOne({$pull: { images: { filename: {$in: req.body.deleteImages }}}})
-            }
-            req.flash("success", 'Successfully Updated the Campground!');
-            res.redirect(`/campgrounds/${id}`);
-    }
-    
-    module.exports.showCampground = async (req, res, next) => {
-            const {id} = req.params;
-            if (!id) throw new generateError(400, "Missing/Invalid ID.")
-            const campground = await Campground.findById(id).populate({
-                path:"reviews",
-                populate: { 
-                    path: "author"
-                    }
-                }).populate('author','username email');
-            if (!campground) 
-            {   
-                req.flash('error',"Campground not Found!");
-                return res.redirect("/campgrounds");
-            }    
-            res.render("campgrounds/show",{campground});
-    }
-})(
-    require('../models/campground'),
-    require('../utilities/generateError'),
-    require('../cloudinary/index'),
-    require('@mapbox/mapbox-sdk/services/geocoding')
-)
+    req.flash("success", 'Successfully Updated the Campground!');
+    res.redirect(`/campgrounds/${id}`);
+}    
+module.exports.showCampground = async (req, res, next) => {
+    const {id} = req.params;
+    if (!id) throw new generateError(400, "Missing/Invalid ID.")
+    const campground = await Campground.findById(id).populate({
+        path:"reviews",
+        populate: { 
+            path: "author"
+        }
+        }).populate('author','username email');
+    if (!campground) 
+    {   
+        req.flash('error',"Campground not Found!");
+        return res.redirect("/campgrounds");
+    }    
+    res.render("campgrounds/show",{campground});
+}
--- controllers/user.js
+++ controllers/user.js
@@ -1,10 +1,9 @@
-((User) => {
+const User = require('../models/user');
 
-    module.exports.redirectToRegister = (req, res) => {
-            res.render("users/register");
-    }
-    
-    module.exports.registerUser = async (req, res) => {
+module.exports.redirectToRegister = (req, res) => {
+        res.render("users/register");
+}    
+module.exports.registerUser = async (req, res) => {
         try{
                 const { user:userData } = req.body;
                 const user = new User({username: userData.username, email: userData.email});
@@ -14,30 +13,23 @@
                     req.flash('success',"Welcome to LBCamp!")
                     res.redirect("/campgrounds");
                 });
-            }
+        }
         catch(e){
                 req.flash("error", e.message);
                 return res.redirect("/register");
-            }
-    }
-    
-    module.exports.redirectToLogin = (req, res) => {
-            res.render("users/login");
-    }
-    
-    module.exports.logUserIn = async (req, res) => {
-            req.flash('success',"Welcome Back!")
-            const redirect = req.session.returnTo || "/campgrounds";
-            delete req.session.returnTo
-            res.redirect(redirect);
-    }
-    
-    module.exports.logUserOut = (req, res) => {
-            req.logOut();
-            req.flash("success", 'See You Again!');
-            res.redirect("/campgrounds");
-    }
-
-})(
-    require('../models/user')
-)
+        }
+}
+module.exports.redirectToLogin = (req, res) => {
+        res.render("users/login");
+}
+module.exports.logUserIn = async (req, res) => {
+        req.flash('success',"Welcome Back!")
+        const redirect = req.session.returnTo || "/campgrounds";
+        delete req.session.returnTo
+        res.redirect(redirect);
+}
+module.exports.logUserOut = (req, res) => {
+        req.logOut();
+        req.flash("success", 'See You Again!');
+        res.redirect("/campgrounds");
+}
